4fe70dd4285849dfb71b040fdcbef9b9ea49390d
spindly <kaihrmt@gmail.com>
2024-01-11 22:50:22 +0000

    placed data clean up into a function

--- imageClassification/imageManipulation.py
+++ imageClassification/imageManipulation.py
@@ -3,19 +3,18 @@ import imghdr
 import os
 # this file is used to clean up image data, removing bad images (currupted/inaccessable) from downloaded data online
 
-data_dir = "data" # points image directory 
-vaild_extention = ["jpeg","jpg","bmp","png"]
+def image_cleanup(data_dir):
+    vaild_extention = ["jpeg","jpg","bmp","png"]
+    for folder in os.listdir(data_dir):
+        folder_path = os.path.join(data_dir,folder)
+        for image in os.listdir(folder_path):
+            image_path = os.path.join(folder_path,image)
+            try:
+                attempt_image_open = cv2.imread(image_path) # attempts to open file
+                extention = imghdr.what(image_path) # retrives file type
+                if extention not in vaild_extention:
+                    print(f"file : {image} is not of vaild file type ({vaild_extention})")
+                    os.remove(image_path)
+            except Exception as e:
+                print(f"exception: {e} \n raised on image: {image} in {folder_path}")
 
-for folder in os.listdir(data_dir):
-    folder_path = os.path.join(data_dir,folder)
-    for image in os.listdir(folder_path):
-        image_path = os.path.join(folder_path,image)
-        try:
-            attempt_image_open = cv2.imread(image_path) # attempts to open file
-            extention = imghdr.what(image_path) # retrives file type
-            if extention not in vaild_extention:
-                print(f"file : {image} is not of vaild file type ({vaild_extention})")
-                os.remove(image_path)
-        except Exception as e:
-            print(f"exception: {e} \n raised on image: {image} in {folder_path}")
-            
